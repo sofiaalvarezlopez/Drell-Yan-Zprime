127344f3a829daf5141cd807775c1662edfb7a26
Sofia Alvarez <ms.alvarezl@uniandes.edu.co>
2021-11-16 22:01:59 -0500

    Deleted extra ~

--- pyroot.py
+++ pyroot.py
@@ -49,7 +49,7 @@ signals = ["Zprime_tata_350", "Zprime_tata_1000", "Zprime_tata_3000"]
 #ext = ["/disco1/SIMULACIONES/", "falta", "/disco2/disco3_ORG/SIMULACIONES/",
 #       "/disco2/disco3_ORG/SIMULACIONES/", "/disco2/disco3_ORG/SIMULACIONES/", "/disco2/disco3_ORG/SIMULACIONES/"]
 ext = ["~/SIMULACIONES/Sofia/", "~/SIMULACIONES/Sofia/", "~/SIMULACIONES/Sofia/"]
-sufijos = ["tag_1_delphes_events.root", "tag_1_delphes_events~.root", "tag_1_delphes_events.root"]
+sufijos = ["tag_1_delphes_events.root", "tag_1_delphes_events.root", "tag_1_delphes_events.root"]
 #sufijos = ["m_delphes_events.root", "m_delphes_events.root", "m_delphes_events.root", "m_delphes_events.root", "m_delphes_events.root",
 #           "tag_1_delphes_events.root"]
 jobs = [2,2,2]
